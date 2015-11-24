ed31a729e9f93bb02bfaa233a7d1188c13ddf748
jacobben85 <jacobben85@gmail.com>
2015-11-24 16:36:19 -0500

    Few more validations

--- src/main/java/com/jbjohn/calendar/Validation.java
+++ src/main/java/com/jbjohn/calendar/Validation.java
@@ -3,6 +3,7 @@ package com.jbjohn.calendar;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import org.joda.time.DateTime;
 
 /**
  * Validating calendar
@@ -20,5 +21,40 @@ public class Validation {
 
         String tsExpire = formatter.format(now.getTimeInMillis());
         System.out.println(tsExpire);
+
+        int delayHours = -13;
+
+        DateTime today = getStartDate(delayHours);
+        DateTime tomorrow = getEndDate(delayHours);
+
+        System.out.println(new DateTime());
+        System.out.println(new DateTime().withTimeAtStartOfDay());
+        System.out.println(today);
+        System.out.println(tomorrow);
+    }
+
+    private DateTime getStartDate(int delayHours) {
+        DateTime today = new DateTime();
+
+        if (delayHours >= 0) {
+            today = today.minusHours(delayHours);
+        }
+
+        today = today.withTimeAtStartOfDay();
+
+        if (delayHours >= 0) {
+            today = today.plusHours(delayHours);
+        } else {
+            today = today.minusHours(0 - delayHours);
+        }
+
+        return today;
+    }
+
+    private DateTime getEndDate(int delayHours) {
+        DateTime today = getStartDate(delayHours);
+
+        DateTime tomorrow = today.plusDays(1);
+        return tomorrow;
     }
 }
