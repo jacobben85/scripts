c42cf05485047eb65891f51791d8da7ee4585ef2
jacobben85 <jacobben85@gmail.com>
2015-12-08 12:52:58 -0500

    Adding the schedule time

--- src/main/java/com/jbjohn/calendar/EnVivo.java
+++ src/main/java/com/jbjohn/calendar/EnVivo.java
@@ -1,5 +1,6 @@
 package com.jbjohn.calendar;
 
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -10,6 +11,9 @@ public class EnVivo {
     public void process(Date matchTime) {
 
         Date date = new Date();
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        Date scheduleDate;
 
         if (matchTime.after(date)) {
             long difference = matchTime.getTime() - date.getTime();
@@ -17,13 +21,19 @@ public class EnVivo {
             long differenceMin = (difference / (60 * 1000));
             if (differenceMin > 20) {
                 long schedule = differenceMin - 20;
-                System.out.println("Schedule alert : " + schedule + ", start in 20");
+                cal.add(Calendar.MINUTE, Long.valueOf(schedule).intValue());
+                scheduleDate = cal.getTime();
+                System.out.println("Schedule alert : " + scheduleDate + ", start in 20");
             } else if (differenceMin > 10) {
                 long schedule = differenceMin - 10;
-                System.out.println("Schedule alert : " + schedule + ", start in 10");
+                cal.add(Calendar.MINUTE, Long.valueOf(schedule).intValue());
+                scheduleDate = cal.getTime();
+                System.out.println("Schedule alert : " + scheduleDate + ", start in 10");
             } else if (differenceMin > 5) {
                 long schedule = differenceMin - 5;
-                System.out.println("Schedule alert : " + schedule + ", start in 5");
+                cal.add(Calendar.MINUTE, Long.valueOf(schedule).intValue());
+                scheduleDate = cal.getTime();
+                System.out.println("Schedule alert : " + scheduleDate + ", start in 5");
             } else {
                 System.out.println("Event start soon");
             }
